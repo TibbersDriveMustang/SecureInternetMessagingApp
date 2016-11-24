dfb565021c913ca022b9eb68e1a1df217d82913b
Hongyi Guo <guohongyi1991@gmail.com>
2016-11-23 22:13:59 -0600

    client pwd compare done

--- Client.py
+++ Client.py
@@ -1,6 +1,6 @@
 import socket
 import sys
-import Crypto
+from Crypto.Hash import SHA256
 from Tkinter import *
 
 class commands:
@@ -12,7 +12,6 @@ class Client:
     def __init__(self):
         #Create a TCP/IP socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
         #Password GUI
         self.master = Tk()
         #Password input column
@@ -24,6 +23,10 @@ class Client:
         self.button["text"] = "Enter"
         self.button["command"] = self.comparePWD
         self.button.grid(row = 1, column = 0, padx = 2, pady = 2)
+
+
+        #Password
+        self.passwordHash = SHA256.new('hxg140630').digest()
 
         #Connect the socket to the port where the server is listening
         server_address = ('localhost', 10000)
@@ -39,29 +42,35 @@ class Client:
             self.sock.send(command)
 
             # Send data
-            message = 'Client Password: ' + self.password
+            message = 'Client Session Key: ' # session key
             print >> sys.stderr, 'sending "%s"' % message
             self.sock.sendall(message)
 
             #Look for the response
             amount_received = 0
             amount_expected = len(message)
-
+            receiver = ''
             while amount_received < amount_expected:
                 data = self.sock.recv(16)
+                receiver += data
                 amount_received += len(data)
-                print >> sys.stderr, 'received "%s"' % data
+
+            print >> sys.stderr, 'received "%s"' % data
 
         finally:
             print >> sys.stderr, 'closing socket'
             self.sock.close()
 
     def comparePWD(self):
-        self.password = self.e1.get()
-        print >> sys.stderr, self.password
+        password = self.e1.get()
+        passwordHash = SHA256.new(password).digest()
         self.master.quit()
-        print >> sys.stderr, 'Compare Password'
+        print >> sys.stderr, 'Comparing Password'
 
+        if passwordHash == self.passwordHash:
+            print >> sys.stderr, 'User Authenticated'
+        else:
+            sys.exit("Password Error")
 
 #    def authentication(self):
 
